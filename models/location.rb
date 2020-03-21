8d2b2603f688c98ef7907378f4a1b0b877bf40b9
Harmandeep Mand <harmandeepmand.hm@gmail.com>
2020-03-21 01:57:41 -0400

    minor repairs to go

--- models/location.rb
+++ models/location.rb
@@ -4,19 +4,38 @@ class Location
     def initialize
         res = RestClient.get('https://supercharge.info/service/supercharge/sites?draw=3&columns%5B0%5D%5Bdata%5D=name&columns%5B0%5D%5Bname%5D=&columns%5B0%5D%5Bsearchable%5D=true&columns%5B0%5D%5Borderable%5D=true&columns%5B0%5D%5Bsearch%5D%5Bvalue%5D=&columns%5B0%5D%5Bsearch%5D%5Bregex%5D=false&columns%5B1%5D%5Bdata%5D=address.street&columns%5B1%5D%5Bname%5D=&columns%5B1%5D%5Bsearchable%5D=true&columns%5B1%5D%5Borderable%5D=true&columns%5B1%5D%5Bsearch%5D%5Bvalue%5D=&columns%5B1%5D%5Bsearch%5D%5Bregex%5D=false&columns%5B2%5D%5Bdata%5D=address.city&columns%5B2%5D%5Bname%5D=&columns%5B2%5D%5Bsearchable%5D=true&columns%5B2%5D%5Borderable%5D=true&columns%5B2%5D%5Bsearch%5D%5Bvalue%5D=&columns%5B2%5D%5Bsearch%5D%5Bregex%5D=false&columns%5B3%5D%5Bdata%5D=address.state&columns%5B3%5D%5Bname%5D=&columns%5B3%5D%5Bsearchable%5D=true&columns%5B3%5D%5Borderable%5D=true&columns%5B3%5D%5Bsearch%5D%5Bvalue%5D=&columns%5B3%5D%5Bsearch%5D%5Bregex%5D=false&columns%5B4%5D%5Bdata%5D=address.zip&columns%5B4%5D%5Bname%5D=&columns%5B4%5D%5Bsearchable%5D=true&columns%5B4%5D%5Borderable%5D=true&columns%5B4%5D%5Bsearch%5D%5Bvalue%5D=&columns%5B4%5D%5Bsearch%5D%5Bregex%5D=false&columns%5B5%5D%5Bdata%5D=address.country&columns%5B5%5D%5Bname%5D=&columns%5B5%5D%5Bsearchable%5D=true&columns%5B5%5D%5Borderable%5D=true&columns%5B5%5D%5Bsearch%5D%5Bvalue%5D=&columns%5B5%5D%5Bsearch%5D%5Bregex%5D=false&columns%5B6%5D%5Bdata%5D=stallCount&columns%5B6%5D%5Bname%5D=&columns%5B6%5D%5Bsearchable%5D=true&columns%5B6%5D%5Borderable%5D=true&columns%5B6%5D%5Bsearch%5D%5Bvalue%5D=&columns%5B6%5D%5Bsearch%5D%5Bregex%5D=false&columns%5B7%5D%5Bdata%5D=powerKilowatt&columns%5B7%5D%5Bname%5D=&columns%5B7%5D%5Bsearchable%5D=true&columns%5B7%5D%5Borderable%5D=true&columns%5B7%5D%5Bsearch%5D%5Bvalue%5D=&columns%5B7%5D%5Bsearch%5D%5Bregex%5D=false&columns%5B8%5D%5Bdata%5D=function&columns%5B8%5D%5Bname%5D=&columns%5B8%5D%5Bsearchable%5D=true&columns%5B8%5D%5Borderable%5D=true&columns%5B8%5D%5Bsearch%5D%5Bvalue%5D=&columns%5B8%5D%5Bsearch%5D%5Bregex%5D=false&columns%5B9%5D%5Bdata%5D=elevationMeters&columns%5B9%5D%5Bname%5D=&columns%5B9%5D%5Bsearchable%5D=true&columns%5B9%5D%5Borderable%5D=true&columns%5B9%5D%5Bsearch%5D%5Bvalue%5D=&columns%5B9%5D%5Bsearch%5D%5Bregex%5D=false&columns%5B10%5D%5Bdata%5D=function&columns%5B10%5D%5Bname%5D=&columns%5B10%5D%5Bsearchable%5D=true&columns%5B10%5D%5Borderable%5D=true&columns%5B10%5D%5Bsearch%5D%5Bvalue%5D=&columns%5B10%5D%5Bsearch%5D%5Bregex%5D=false&columns%5B11%5D%5Bdata%5D=dateOpened&columns%5B11%5D%5Bname%5D=&columns%5B11%5D%5Bsearchable%5D=true&columns%5B11%5D%5Borderable%5D=true&columns%5B11%5D%5Bsearch%5D%5Bvalue%5D=&columns%5B11%5D%5Bsearch%5D%5Bregex%5D=false&columns%5B12%5D%5Bdata%5D=function&columns%5B12%5D%5Bname%5D=&columns%5B12%5D%5Bsearchable%5D=true&columns%5B12%5D%5Borderable%5D=false&columns%5B12%5D%5Bsearch%5D%5Bvalue%5D=&columns%5B12%5D%5Bsearch%5D%5Bregex%5D=false&order%5B0%5D%5Bcolumn%5D=10&order%5B0%5D%5Bdir%5D=desc&start=0&length=10000&search%5Bvalue%5D=&search%5Bregex%5D=false&regionId=&countryId=&_=1583645074736')
         @res_hash = JSON.parse(res)
+        mychargers = MySuperchargers.all
+        mychargers.each do |sc|
+            hash = {
+                "locationId" => "#{sc.name.downcase}supercharger",
+                "name" => sc.name,
+                "address" => {
+                    "street" => sc.street,
+                    "city" => sc.city,
+                    "state" => sc.state,
+                    "zip" => sc.zip,
+                    "country" =>sc.country
+                },
+                "stallCount" => sc.stalls,
+                "powerKiloatt" => sc.power,
+                "gps" => {
+                    "latitude" => sc.latitude.to_f,
+                    "longitude" => sc.longitude.to_f
+                }
+            }
+            @res_hash["results"] << hash
+        end
     end
 
     def find(str)
         findcharger = {}
         allstates = []
         strcoor = Geocoder.coordinates(str)
-        binding.pry
         @res_hash["results"].each do |charger|
-            checkstate = Integer(charger["address"]["state"]) rescue false
-            if charger["address"]["city"].to_s.downcase == str.downcase || charger["address"]["zip"].to_s.downcase == str.downcase || charger["address"]["state"].to_s.downcase == str.downcase || 
+            if charger["address"]["city"].to_s.downcase == str.downcase || charger["address"]["zip"].to_s.downcase == str.downcase || charger["address"]["state"].to_s.downcase == str.downcase 
                 charger_location = ["#{charger["gps"]["latitude"]}","#{charger["gps"]["longitude"]}"]
-                search_locatiion = ["#{strcoor[0]}","#{strcoor[1]}"]
-                distance = dist_cal([search_locatiion[0].to_f,search_locatiion[1].to_f],[charger_location[0].to_f,charger_location[1].to_f])
+                search_location = ["#{strcoor[0]}","#{strcoor[1]}"]
+                distance = dist_cal([search_location[0].to_f,search_location[1].to_f],[charger_location[0].to_f,charger_location[1].to_f])
                 findcharger[charger["locationId"]] = {
                     "name" => charger["name"],
                     "street" => charger["address"]["street"],
